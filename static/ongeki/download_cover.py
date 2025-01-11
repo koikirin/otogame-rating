cfd83fcbbd646ddf0bd8c2d56f4bb2d3e8fea6cb
Hieuzest <girkirin@hotmail.com>
2025-01-12 04:25:04 +0800

    feat: update download script

--- static/ongeki/download_cover.py
+++ static/ongeki/download_cover.py
@@ -3,16 +3,22 @@ import asyncio
 import aiohttp
 import os
 
+PROXY = None
+try:
+    with open('../PROXY', 'r') as f:
+        PROXY = f.read().strip()
+except:
+    pass
+
 with open('data.json', 'r') as f:
     data = json.load(f)
-
 
 async def download(url, path):
     try:
         async with sess.get(url, headers = {
             "Uesr-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-        }, timeout = 5, proxy = "socks://127.0.0.1:7890") as res:
+        }, timeout = 5, proxy = PROXY) as res:
             with open(path, 'wb') as f:
                 f.write(await res.read())
     except Exception as e:
@@ -24,7 +30,7 @@ async def main():
         for song in data["songs"]:
             img = song["imageName"]
             if os.path.exists(f'cover_ori/{img}'):
-                print('skip', img)
+                # print('skip', img)
                 continue
             url = f'https://dp4p6x0xfi5o9.cloudfront.net/ongeki/img/cover-m/{img}'
             print(url)
--- static/ongeki/download_data.py
+++ static/ongeki/download_data.py
@@ -1,10 +1,17 @@
 import asyncio
 import aiohttp
 
+PROXY = None
+try:
+    with open('../PROXY', 'r') as f:
+        PROXY = f.read().strip()
+except:
+    pass
+
 async def main():
     url = 'https://dp4p6x0xfi5o9.cloudfront.net/ongeki/data.json'
     async with aiohttp.ClientSession() as sess:
-        async with sess.get(url, proxy = "socks://127.0.0.1:7890") as res:
+        async with sess.get(url, proxy = PROXY) as res:
             with open('data.json', 'wb') as f:
                 f.write(await res.read())
 
